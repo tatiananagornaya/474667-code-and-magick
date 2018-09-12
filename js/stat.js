1fbc177d18ba0bc9f5dfed898adec05a1d1da26c
Tanya <tati.nagornaya@gmail.com>
2018-09-12 22:35:17 +0300

    Задание 2: разделение на разн.функции отрисовки облака

--- js/stat.js
+++ js/stat.js
@@ -14,15 +14,15 @@ var barWidth = TEXT_WIDTH;
 var textX = CLOUD_X * 1.5;
 var textY = CLOUD_Y * 4;
 
+var renderCloud = function(ctx, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+};
+
 var renderCongrats = function (ctx, text, x, y) {
   ctx.fillStyle = 'black';
   ctx.font = '16 px PT Mono';
   ctx.fillText(text, x, y);
-};
-
-var renderCloud = function(ctx, x, y, color) {
-  ctx.fillStyle = color;
-  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
  var getMaxValue = function(times) {
@@ -34,12 +34,31 @@ var renderCloud = function(ctx, x, y, color) {
    }
    return maxValue;
  };
+ 
+var renderText = function (ctx, names) {
+  renderCongrats(ctx, 'Ура вы победили!', textX, textY);
+  renderCongrats(ctx, 'Список результатов:', textX, textY * 1.5);
+
+  ctx.fillStyle = 'black';
+  for (var j = 0; j < names.length; j++) {
+    ctx.fillText(names[j], (CLOUD_X + FONT_GAP) + TEXT_WIDTH * j + FONT_GAP * j, BAR_NAME_Y);
+  }
+};
+
+var renderFigures = function (ctx, times) {
+  var maxTime = getMaxValue(times);
+
+  ctx.fillStyle = 'black';
+  for (var z = 0; z < times.length; z++) {
+    ctx.fillText(Math.round(times[z]), (CLOUD_X + FONT_GAP) + TEXT_WIDTH * z + FONT_GAP * z, BAR_NAME_Y - (times[z] * BAR_HEIGHT / maxTime) - GAP * 2 );
+  }
+};
 
 window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, 'white');
-  renderCongrats(ctx, 'Ура вы победили!', textX, textY);
-  renderCongrats(ctx, 'Список результатов:', textX, textY * 1.5);
+  renderText(ctx, names);
+  renderFigures(ctx, times);
 
   var maxTime = getMaxValue(times);
   var yourResultIndex = names.indexOf('Вы');
@@ -47,18 +66,9 @@ window.renderStatistics = function (ctx, names, times) {
   for (var i = 0; i < names.length; i++) {
     if (i === yourResultIndex) {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-      ctx.fillText(names[i], (CLOUD_X + FONT_GAP) + TEXT_WIDTH * i + FONT_GAP * i, BAR_NAME_Y);
-      ctx.fillRect((CLOUD_X + FONT_GAP) + TEXT_WIDTH * i + FONT_GAP * i, BAR_Y - (times[i] * BAR_HEIGHT / maxTime), barWidth, times[i] * BAR_HEIGHT / maxTime);
-
   } else {
       ctx.fillStyle = 'blue';
-      ctx.fillText(names[i], (CLOUD_X + FONT_GAP) + TEXT_WIDTH * i + FONT_GAP * i, BAR_NAME_Y);
-      ctx.fillRect((CLOUD_X + FONT_GAP) + TEXT_WIDTH * i + FONT_GAP * i, BAR_Y - (times[i] * BAR_HEIGHT / maxTime), barWidth, times[i] * BAR_HEIGHT / maxTime);
     }
-  };
-
-  for (var i = 0; i < times.length; i++){
-    ctx.fillStyle = 'black';
-    ctx.fillText(Math.round(times[i]), (CLOUD_X + FONT_GAP) + TEXT_WIDTH * i + FONT_GAP * i, BAR_NAME_Y - (times[i] * BAR_HEIGHT / maxTime) - GAP * 2 );
+    ctx.fillRect((CLOUD_X + FONT_GAP) + TEXT_WIDTH * i + FONT_GAP * i, BAR_Y - (times[i] * BAR_HEIGHT / maxTime), barWidth, times[i] * BAR_HEIGHT / maxTime);
   }
 };
