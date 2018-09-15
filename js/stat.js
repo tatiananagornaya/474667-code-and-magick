91599f4eb2c591b4cdc0941dc339ce7795c5475a
Tanya <tati.nagornaya@gmail.com>
2018-09-16 00:22:42 +0300

    Задание 2: правки

--- js/stat.js
+++ js/stat.js
@@ -31,16 +31,9 @@ var renderCloud = function(ctx, x, y, color) {
    return maxValue;
  };
 
-var renderText = function (ctx, names, times) {
+var renderText = function (ctx, names, times, x, y) {
   var maxTime = getMaxValue(times);
-
-  ctx.fillStyle = 'rgb(0, 0, 0)';
-  ctx.fillText(names[j], (CLOUD_X + FONT_GAP) + TEXT_WIDTH * j + FONT_GAP * j, BAR_NAME_Y);
-  ctx.fillText(Math.round(times[j]), (CLOUD_X + FONT_GAP) + TEXT_WIDTH * j + FONT_GAP * j, BAR_NAME_Y - (times[j] * BAR_HEIGHT / maxTime) - GAP * 2 );
-
-    ctx.font = '16px PT Mono';
-    ctx.fillText(text[0], TEXT_X, TEXT_Y);
-    ctx.fillText(text[1], TEXT_X, TEXT_Y * 1.5);
+  ctx.fillText(text, x, y);
 };
 
 var getRandomColor = function () {
@@ -50,12 +43,19 @@ var getRandomColor = function () {
 window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, 'rgb(255, 255, 255)');
+  var maxTime = getMaxValue(times);
 
   for (j = 0; j < names.length; j++) {
+    ctx.fillStyle = 'rgb(0, 0, 0)';
+    ctx.fillText(names[j], (CLOUD_X + FONT_GAP) + TEXT_WIDTH * j + FONT_GAP * j, BAR_NAME_Y);
+    ctx.fillText(Math.round(times[j]), (CLOUD_X + FONT_GAP) + TEXT_WIDTH * j + FONT_GAP * j, BAR_NAME_Y - (times[j] * BAR_HEIGHT / maxTime) - GAP * 2 );
+
+    ctx.font = '16px PT Mono';
+    ctx.fillText(text[0], TEXT_X, TEXT_Y);
+    ctx.fillText(text[1], TEXT_X, TEXT_Y * 1.5);
     renderText(ctx, names, times);
   }
 
-  var maxTime = getMaxValue(times);
   var color = getRandomColor();
 
   for (var i = 0; i < names.length; i++) {
